215e459b54bca01e3164044b72003768c999d06c
Austin Huang <huangaustin22@gmail.com>
2026-09-11 14:42:08 +0000

    complete challenge 4

--- genai-jam/challenges/upgrade-required/src/main/java/com/conygre/spring/boot/rest/CompactDiscController.java
+++ genai-jam/challenges/upgrade-required/src/main/java/com/conygre/spring/boot/rest/CompactDiscController.java
@@ -2,7 +2,7 @@ package com.conygre.spring.boot.rest;
 
 import com.conygre.spring.boot.services.CompactDiscService;
 import com.conygre.spring.boot.entities.CompactDisc;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 //import org.apache.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -23,19 +23,21 @@ public class CompactDiscController {
 	@Autowired
 	private CompactDiscService service;
 
-	@ApiOperation(value = "findAll", nickname = "findAll")
-	@RequestMapping(method = RequestMethod.GET)
+	@Operation(summary = "Get all compact discs", description = "Retrieve the complete catalog of compact discs")
+	@GetMapping
 	public Iterable<CompactDisc> findAll() {
 		logger.info("managed to call a Get request for findAll");
 		return service.getCatalog();
 	}
 
-	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
+	@Operation(summary = "Get compact disc by ID")
+	@GetMapping("/{id}")
 	public CompactDisc getCdById(@PathVariable("id") int id) {
 		return service.getCompactDiscById(id);
 	}
 
-	@RequestMapping(method=RequestMethod.GET, value="/404/{id}")
+	@Operation(summary = "Get compact disc by ID with 404 handling")
+	@GetMapping("/404/{id}")
 	public ResponseEntity<CompactDisc> getByIdWith404(@PathVariable("id") int id) {
 		CompactDisc disc = service.getCompactDiscById(id);
 		if (disc == null) {
@@ -46,18 +48,20 @@ public class CompactDiscController {
 		}
 	}
 
-
-	@RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+	@Operation(summary = "Delete compact disc by ID")
+	@DeleteMapping("/{id}")
 	public void deleteCd(@PathVariable("id") int id) {
 		service.deleteCompactDisc(id);
 	}
 
-	@RequestMapping(method = RequestMethod.DELETE)
+	@Operation(summary = "Delete compact disc by object")
+	@DeleteMapping
 	public void deleteCd(@RequestBody CompactDisc disc) {
 		service.deleteCompactDisc(disc);
 	}
 
-	@RequestMapping(method = RequestMethod.POST)
+	@Operation(summary = "Add new compact disc")
+	@PostMapping
 	public void addCd(@RequestBody CompactDisc disc) {
 		service.addNewCompactDisc(disc);
 	}
